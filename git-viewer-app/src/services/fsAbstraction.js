38791baa2b47480875cc982ba8bdacc936e68ed7
Git Viewer User <user@gitviewer.app>
2025-09-11 14:44:47 +0300

    normalize paths

--- git-viewer-app/src/services/fsAbstraction.js
+++ git-viewer-app/src/services/fsAbstraction.js
@@ -8,56 +8,52 @@ class FSAbstraction {
     this.directoryHandle = options.directoryHandle || null;
     this.fs = new FsaNodeFs(this.directoryHandle);
     this.pfs = this.fs.promises;
+
+    // Dynamically create thin wrapper methods that:
+    // 1. Normalize specified argument indexes (usually the path at index 0, symlink uses index 1)
+    // 2. Call the underlying promise fs method
+    // 3. Log AFTER normalization to reflect the actual values passed
+    // This removes a lot of boilerplate while keeping explicit method names.
+    const methodPathArgMap = {
+      readdir: [0],
+      lstat: [0],
+      readlink: [0],
+      symlink: [1], // (target, path) => need to normalize the path (2nd arg)
+      stat: [0],
+      readFile: [0],
+      writeFile: [0],
+      mkdir: [0],
+      rmdir: [0],
+      unlink: [0],
+    };
+
+    Object.entries(methodPathArgMap).forEach(([name, pathIndexes]) => {
+      // Only create if not already defined on instance (allows future overrides)
+      if (!this[name]) {
+        this[name] = async (...args) => {
+          if (Array.isArray(pathIndexes)) {
+            for (const idx of pathIndexes) {
+              if (idx != null && args[idx] !== undefined) {
+                args[idx] = this.normalize(args[idx]);
+              }
+            }
+          }
+          // Log after normalization so we can see the actual values being passed
+          console.log(name, args);
+          return this.pfs[name](...args);
+        };
+      }
+    });
   }
 
-  async readdir() {
-    console.log('readdir', arguments);
-    return this.pfs.readdir.apply(this.pfs, arguments);
-  }
+  // (Legacy note) Individual async methods were replaced by dynamic generation in constructor.
 
-  async lstat() {
-    console.log('lstat', arguments);
-    return this.pfs.lstat.apply(this.pfs, arguments);
-  }
-
-  async readlink() {
-    console.log('readlink', arguments);
-    return this.pfs.readlink.apply(this.pfs, arguments);
-  }
-
-  async symlink() {
-    console.log('symlink', arguments);
-    return this.pfs.symlink.apply(this.pfs, arguments);
-  }
-
-  async stat() {
-    console.log('stat', arguments);
-    return this.pfs.stat.apply(this.pfs, arguments);
-  }
-
-  async readFile() {
-    console.log('readFile', arguments);
-    return this.pfs.readFile.apply(this.pfs, arguments);
-  }
-
-  async writeFile() {
-    console.log('writeFile', arguments);
-    return this.pfs.writeFile.apply(this.pfs, arguments);
-  }
-
-  async mkdir() {
-    console.log('mkdir', arguments);
-    return this.pfs.mkdir.apply(this.pfs, arguments);
-  }
-
-  async rmdir() {
-    console.log('rmdir', arguments);
-    return this.pfs.rmdir.apply(this.pfs, arguments);
-  }
-
-  async unlink() {
-    console.log('unlink', arguments);
-    return this.pfs.unlink.apply(this.pfs, arguments);
+  normalize(path) {
+    if (typeof path !== "string") {
+      return path;
+    }
+    // make sure no leading ".". no, double slashes, etc
+    return path.replace(/\\/g, "/").replace(/\/+/g, "/").replace(/\/\.$/, "");
   }
 
   async isGitRepository() {
